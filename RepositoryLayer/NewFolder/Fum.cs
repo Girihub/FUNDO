7bf3873bc405ca1cd3c6eb949210740c733a5244
Girihub <giridhardandikwar@gmail.com>
2019-11-24 13:03:10 +0530

    commit

---
+++ RepositoryLayer/NewFolder/Fum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.NewFolder
+{
+    public class Fum
+    {
+        public static bool fun()
+        {
+            return true;
+        }
+    }
+}

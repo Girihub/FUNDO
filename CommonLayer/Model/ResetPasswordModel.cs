229aaa396ec7963f77395acc5a2fbaa031f380ed
Girihub <giridhardandikwar@gmail.com>
2019-11-24 13:06:38 +0530

    commit

--- CommonLayer/Model/ResetPasswordModel.cs
+++ CommonLayer/Model/ResetPasswordModel.cs
@@ -4,10 +4,26 @@
 // </copyright>
 //----------------------------------------------------
 
+using System.ComponentModel.DataAnnotations;
+
 namespace CommonLayer.Model
 {
     public class ResetPasswordModel
     {
+        /// <summary>
+        /// Gets or sets Email of user 
+        /// </summary>
+        /// Set the field compulsory to fill by annotation
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression("^([a-z0-9](.?[a-z0-9]){4,}@g(oogle)?mail.com)$", ErrorMessage = "Enter valid gmail. eg. giri.123@gmail.com")]
+        public string Email { get; set; }
 
+        /// <summary>
+        /// Gets or sets Password of user 
+        /// </summary>
+        /// Set the field compulsory to fill by annotation
+        [Required(ErrorMessage = "Password?")]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*_)(?=.*[A-Z]).{4,8}$", ErrorMessage = "Password should be of 4 to 8 letters having atleast 1 upper case letter, 1 lower case letter, 1 digit and 1 _")]
+        public string NewPassword { get; set; }
     }
 }
